a6f7d19e07efea00f2e832d51f4ddadbf2abbe2a
namtran1205 <nguyenhanamtranhv1205@gmail.com>
2024-07-18 21:02:27 +0700

    Finish BFS, DFS, GBFS
    Co-Authored-By: Đặng Triệu Kha <126672405+DangTrieuKha@users.noreply.github.com>

--- agent.py
+++ agent.py
@@ -1,17 +1,62 @@
 from board import *
 from queue import PriorityQueue
-
+from collections import deque
 class PlayerLvl1:
     def __init__(self):
         pass
 
-    def BFS(self, board):
+    def BFS(self, board: Board):
         """
         input: board: list(list()), a 2D list representing the map
         output: result: list((x, y)), a list of strings representing the moves on the coordinate
         delete pass statement before implementing
         """
-        pass
+        goal = board.findEnd()
+        start = board.findStart()
+        x_movement = [1, -1, 0, 0]
+        y_movement = [0, 0, -1, 1]
+        reached = {start}
+        
+        current_node = Node(start[0], start[1])
+        frontier = deque([current_node])
+        if current_node.x == goal[0] and current_node.y == goal[1]:
+            return [start]
+        
+        while frontier:
+            current_node = frontier.popleft()
+            check = False
+            for i in range(4):
+                x = current_node.x + x_movement[i]
+                y = current_node.y + y_movement[i]
+                if board.isValid(x, y) and (x, y) not in reached:
+                    if x == goal[0] and y == goal[1]:
+                        current_node = Node(x, y, current_node)
+                        check = True
+                        break
+                    new_node = Node(x, y, current_node)
+                    frontier.append(new_node)
+                    reached.add((x, y))
+            if check:
+                break
+                    
+        result = []
+        while current_node:
+            result.append((current_node.x, current_node.y))
+            current_node = current_node.parent
+        return result[::-1]
+
+        
+
+    def isCycle(self, node: Node):
+        
+            
+        current = node.parent
+        while current is not None:
+            if current.x == node.x and current.y == node.y:
+                return True
+            current = current.parent
+        return False
+
 
     def DFS(self, board):
         """
@@ -19,7 +64,41 @@ class PlayerLvl1:
         output: result: list((x, y)), a list of strings representing the moves on the coordinate
         delete pass statement before implementing
         """
-        pass
+        goal = board.findEnd()
+        start = board.findStart()
+        current_node = Node(start[0], start[1], None, 0)
+        stack = [current_node]
+        reached = {start: current_node}
+        if current_node.x == goal[0] and current_node.y == goal[1]:
+            return [start]
+        x_movement = [1, -1, 0, 0]
+        y_movement = [0, 0, -1, 1]
+
+        while stack:
+            current_node = stack.pop()
+            check = False
+            for i in range(4):
+                x = current_node.x + x_movement[i]
+                y = current_node.y + y_movement[i]
+                if board.isValid(x, y) and (x, y) not in reached:
+                    if x == goal[0] and y == goal[1]:
+                        current_node = Node(x, y, current_node)
+                        check = True
+                        break
+                    new_node = Node(x, y, current_node)
+                
+                    if not self.isCycle(new_node):
+                        stack.append(new_node)
+                        reached[(x, y)] = new_node
+            if check:
+                break
+        
+        result = []
+        while current_node:
+            result.append((current_node.x, current_node.y))
+            current_node = current_node.parent
+        return result[::-1]
+            
 
     @staticmethod
     def UCS(board: Board):
@@ -66,6 +145,43 @@ class PlayerLvl1:
         output: result: list((x, y)), a list of strings representing the moves on the coordinate
         delete pass statement before implementing
         """
+        def h(node):
+            return abs(node.x - goal[0]) + abs(node.y - goal[1])
+        
+        goal = board.findEnd()
+        start = board.findStart()
+        x_movement = [1, -1, 0, 0]
+        y_movement = [0, 0, -1, 1]
+        reached = {start}
+        
+        current_node = Node(start[0], start[1])
+        frontier = PriorityQueue()
+        frontier.put(( h(current_node), current_node))
+        if current_node.x == goal[0] and current_node.y == goal[1]:
+            return [start]
+        
+        while frontier:
+            priority, current_node = frontier.get()
+            check = False
+            for i in range(4):
+                x = current_node.x + x_movement[i]
+                y = current_node.y + y_movement[i]
+                if board.isValid(x, y) and (x, y) not in reached :
+                    if x == goal[0] and y == goal[1]:
+                        current_node =  Node(x, y, current_node)
+                        check = True
+                        break
+                    new_node = Node(x, y, current_node)
+                    frontier.put(( h(new_node), new_node))
+                    reached.add((x, y))
+            if check:
+                break
+        result = []
+        while current_node:
+            result.append((current_node.x, current_node.y))
+            current_node = current_node.parent
+        return result[::-1]
+
         pass
     
     @staticmethod
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from agent import *
 
 board = Board("input1_level1.txt")
 
-res = PlayerLvl1().AStar(board)
+res = PlayerLvl1().DFS(board)
 
 print(res)
