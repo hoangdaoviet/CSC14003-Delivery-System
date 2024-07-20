7163ddf4150d89cea5a8c58ee983d00b773f3bd0
DangTrieuKha <126672405+DangTrieuKha@users.noreply.github.com>
2024-07-20 20:30:53 +0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -25,7 +25,7 @@ class PlayerLvl1:
             current_node = frontier.popleft()
             # sau khi đánh dấu node đã expanded thì tạm ngừng 1s để vẽ lên màn hình
             board.coloredMap[current_node.x][current_node.y] = 'e' # e for expanded
-            time.sleep(1)
+            
             check = False
             for i in range(4):
                 x = current_node.x + x_movement[i]
@@ -33,7 +33,7 @@ class PlayerLvl1:
                 if board.isValid(x, y) and (x, y) not in reached:
                     # sau khi đánh dấu node đã generated thì tạm ngừng 1s để vẽ lên màn hình
                     board.coloredMap[x][y] = 'g' # g for generated
-                    time.sleep(1)
+                    
                     if x == goal[0] and y == goal[1]:
                         current_node = Node(x, y, current_node)
                         check = True
